2899b52b1d719aa4578da0f94e8161e5768dd0bd
何知谦 <181830072@samil.nju.edu.cn>
2021-09-25 14:28:05 +0800

    commit

---
+++ src/test/java/com/example/testDeploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
