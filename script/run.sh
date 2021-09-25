8873de04cccebd5e037b9168f55e494086812d54
何知谦 <181830072@samil.nju.edu.cn>
2021-09-25 17:03:45 +0800

    a

--- script/run.sh
+++ script/run.sh
@@ -4,4 +4,4 @@ docker rm springboot
 docker rmi springboot
 cd ASEP
 docker build -t springboot .
-docker run -d --name springboot springboot -p 8082:8080 springboot
+docker run -d --name springboot -p 8082:8080 springboot
